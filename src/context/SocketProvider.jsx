0d75f4d8276ef7fa20457109f588a1f013c7bc0e
Parmatma kumar <parmatmakumarr712@gmail.com>
2026-03-28 14:57:08 +0530

    Add URL IN BD

--- src/context/SocketProvider.jsx
+++ src/context/SocketProvider.jsx
@@ -9,7 +9,7 @@ export const useSocket = () => {
 }
 
 export const SocketProvider = (props) => {
-    const socket = useMemo(()=> io('https://whatapp-backend-eta.vercel.app'),[]);
+    const socket = useMemo(()=> io('https://whatapp-backend-gx0y.onrender.com'),[]);
 
   return (
     <SocketContext.Provider value={socket}>
